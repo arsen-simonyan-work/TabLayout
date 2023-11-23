e8677aa5b1614544c92c8e6fc33c1aa13de6ca47
JulietaShahumyan <julieta.shahumyan@gmail.com>
2023-11-23 10:47:49 +0400

    resizable Column

--- src/jvmMain/kotlin/sell/ui/SellUI.kt
+++ src/jvmMain/kotlin/sell/ui/SellUI.kt
@@ -1,20 +1,36 @@
 package sell.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.unit.dp
 import main.BaseViewModel
 import sell.model.ProductGroupListItem
 
 @Composable
 fun CreateSellUi(vm: BaseViewModel) {
-
+    var width by remember { mutableStateOf(250.dp) }
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -23,11 +39,18 @@ fun CreateSellUi(vm: BaseViewModel) {
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .fillMaxWidth(0.3f)
-                        .background(Color(255, 0, 0))
+                        .widthIn(max = width)
+                        .background(Color(255, 0, 0)).pointerInput(Unit) {
+                            detectDragGestures { change, _ ->
+                                width = (width + change.positionChange().x.toDp()).coerceIn(
+                                    250.dp,
+                                    700.dp
+                                )
+                            }
+                        }
                 ) {
                     itemsIndexed((vm as SellViewModel).tasks) { _, item ->
-                       ProductGroupListItem(
+                        ProductGroupListItem(
                             item,
                             checked = item.isExpanded,
                             onChecked = { task ->
