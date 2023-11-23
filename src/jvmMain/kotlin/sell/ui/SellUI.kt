728cd28011b6e170f1529f0a4d42f1f39b606808
JulietaShahumyan <julieta.shahumyan@gmail.com>
2023-11-23 11:38:55 +0400

    resizable content

--- src/jvmMain/kotlin/sell/ui/SellUI.kt
+++ src/jvmMain/kotlin/sell/ui/SellUI.kt
@@ -5,30 +5,23 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.unit.dp
@@ -39,7 +32,6 @@ import sell.model.ProductGroupListItem
 @Composable
 fun CreateSellUi(vm: BaseViewModel, tab: Screen) {
     (vm as SellViewModel).uuid = tab.uuid
-    var width by remember { mutableStateOf(250.dp) }
 
     Surface(
         modifier = Modifier.fillMaxSize()
@@ -49,15 +41,8 @@ fun CreateSellUi(vm: BaseViewModel, tab: Screen) {
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .widthIn(max = width)
-                        .background(Color(255, 0, 0)).pointerInput(Unit) {
-                            detectDragGestures { change, _ ->
-                                width = (width + change.positionChange().x.toDp()).coerceIn(
-                                    250.dp,
-                                    700.dp
-                                )
-                            }
-                        }
+                        .widthIn(max = vm.width)
+                        .background(Color(255, 0, 0))
                 ) {
                     itemsIndexed(vm.tasks) { _, item ->
                         ProductGroupListItem(
@@ -75,6 +60,18 @@ fun CreateSellUi(vm: BaseViewModel, tab: Screen) {
                     }
                 }
             }
+            Spacer(
+                Modifier.fillMaxHeight().width(2.dp).pointerInput(Unit) {
+                    detectDragGestures { change, _ ->
+                        vm.width = (vm.width + change.positionChange().x.toDp()).coerceIn(
+                            250.dp,
+                            700.dp
+                        )
+                    }
+                }.pointerHoverIcon(
+                    icon = PointerIcon.Crosshair
+                )
+            )
             Box(Modifier.fillMaxHeight().fillMaxWidth(1.0f)) {
 //                AsyncImage(
 //                    model = "https://avatars.githubusercontent.com/u/52348172?s=400&u=b966d448e17f4fc8b2ef3fcbc18b395abe21e1c8&v=4",
--- src/jvmMain/kotlin/sell/ui/SellViewModel.kt
+++ src/jvmMain/kotlin/sell/ui/SellViewModel.kt
@@ -1,6 +1,11 @@
 package sell.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.unit.dp
 import main.BaseViewModel
 import sell.model.SellProductGroupModel
 
@@ -8,6 +13,8 @@ class SellViewModel : BaseViewModel() {
     private val _tasks = getProductGroupsList().toMutableStateList()
     val tasks: List<SellProductGroupModel>
         get() = _tasks
+
+    var width by  mutableStateOf(250.dp)
 
     private fun getProductGroupsList() = mutableListOf(
         SellProductGroupModel("Sigaret", 20),
